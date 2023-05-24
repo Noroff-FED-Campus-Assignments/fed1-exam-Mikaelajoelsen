9872b67ecd10ebb7c9b5a811e1c95e76e40bf7a5
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-05-24 12:31:25 +0200

    added images to blog cards

--- js/blog.js
+++ js/blog.js
@@ -1,33 +1,64 @@
-const detailContainer = document.querySelector("#js-results");
+// const detailContainer = document.querySelector("#js-results");
+
+// const url =
+//   "https://mikaela-exam.flywheelsites.com//wp-json/wp/v2/posts/?per_page=30";
+
+// async function fetchresults() {
+//   try {
+//     const response = await fetch(url);
+//     const results = await response.json();
+
+//     console.log(results);
+
+//     makeCards(results);
+//   } catch (error) {
+//     console.log(error);
+//     detailContainer.innerHTML = message("error", error);
+//   }
+// }
+
+// fetchresults();
+
+// function makeCards(postsArray) {
+//   const blogContainer = document.querySelector("#blog-container");
+//   console.log(postsArray);
+//   postsArray.forEach((post) => {
+//     blogContainer.innerHTML += `
+//       <a href="details.html?id=${post.id}" class="container">
+//         <h2>${post.title.rendered}</h2>
+//         <p class="Title">${post.excerpt.rendered}</p>
+//       </a>
+//     `;
+//   });
+// }
 
 const url =
   "https://mikaela-exam.flywheelsites.com//wp-json/wp/v2/posts/?per_page=30";
-
-async function fetchresults() {
+async function fetchResults() {
   try {
     const response = await fetch(url);
     const results = await response.json();
-
-    console.log(results);
-
     makeCards(results);
   } catch (error) {
     console.log(error);
-    detailContainer.innerHTML = message("error", error);
   }
 }
-
-fetchresults();
-
+fetchResults();
+function getImgUrl(someObject) {
+  if (someObject && someObject) {
+    const regex = /<img[^>]+src="([^">]+)"/g;
+    const matches = regex.exec(someObject.rendered);
+    if (matches && matches.length >= 2) {
+      return matches[1];
+    } else {
+      return null;
+    }
+  }
+}
 function makeCards(postsArray) {
   const blogContainer = document.querySelector("#blog-container");
-  console.log(postsArray);
   postsArray.forEach((post) => {
-    blogContainer.innerHTML += `
-      <a href="details.html?id=${post.id}" class="container">
-        <h2>${post.title.rendered}</h2>
-        <p class="Title">${post.excerpt.rendered}</p>
-      </a>
-    `;
+    const imageUrl = getImgUrl(post.content);
+    blogContainer.innerHTML += ` <a href="details.html?id=${post.id}" class="container" style="height: auto;"> <h2>${post.title.rendered}</h2> <p class="Title">${post.excerpt.rendered}</p> <img src="${imageUrl}" alt="blog image" style="height: 324px;"> </a> `;
   });
 }
--- js/contact.js
+++ js/contact.js
@@ -1,72 +1,36 @@
 var myForm = document.querySelector("#js-form-contact");
 myForm.addEventListener("submit", checkFields);
+
 function checkFields(event) {
   event.preventDefault();
-  var is_all_ok = true;
-  let nameRegEx = /^[a-z ,.'-]+$/i;
-  const emailEl = document.querySelector("#js-email");
-  const adressEl = document.querySelector("#js-adress");
-  const nameEl = document.querySelector("#js-name");
-  const birthDateEl = document.querySelector("#js-birthday");
-  const subjectEl = document.querySelector("#js-subject");
+  var isAllOk = true;
+  var nameRegEx = /^[a-zA-Z ,.'-]+$/;
+  var emailEl = document.querySelector("#js-email");
+  var nameEl = document.querySelector("#js-name");
 
-  const name = nameEl.value;
-  const email = emailEl.value;
-  const adress = adressEl.value;
-  const birthday = birthDateEl.value;
-  const subejct = subjectEl.value;
+  var name = nameEl.value.trim();
+  var email = emailEl.value.trim();
   resetValidationFeedback();
-  console.log(birthday);
 
   if (name === "" || !nameRegEx.test(name)) {
-    document.getElementById("nameerrormsg").style.display = "inline";
-    is_all_ok = false;
+    document.getElementById("nameerrormsg").style.display = "block";
+    isAllOk = false;
   }
 
-  if (email === "") {
-    console.log(email);
-    document.getElementById("mailerrormsg").style.display = "inline";
-    is_all_ok = false;
-  }
-  if (adress === "") {
-    document.getElementById("addresserrormsg").style.display = "inline";
-    is_all_ok = false;
+  if (email === "" || !validateEmail(email)) {
+    document.getElementById("mailerrormsg").style.display = "block";
+    isAllOk = false;
   }
 
-  if (birthday === "") {
-    document.getElementById("dateerrormsg").style.display = "inline";
-    is_all_ok = false;
-  } else {
-    var dateProvided = new Date(birthday);
-    var dateToday = new Date();
-    if (
-      dateProvided > dateToday ||
-      dateProvided.toDateString() == dateToday.toDateString()
-    ) {
-      is_all_ok = false;
-      document.getElementById("dateerrormsg").style.display = "inline";
-      document.getElementById("dateerrormsg").textContent +=
-        " Date needs to be a date before the current date";
-    } else if (dateToday.getFullYear() - dateProvided.getFullYear() > 100) {
-      is_all_ok = false;
-      document.getElementById("dateerrormsg").style.display = "inline";
-      document.getElementById("dateerrormsg").textContent +=
-        " You cannot be more than 100 years old.";
-    }
-  }
-  return is_all_ok;
-
-  if (subject === "") {
-    document.getElementById("subjecterrormsg").style.display = "inline";
-    is_all_ok = false;
-  }
+  return isAllOk;
 }
 
 function resetValidationFeedback() {
   document.getElementById("nameerrormsg").style.display = "none";
   document.getElementById("mailerrormsg").style.display = "none";
-  document.getElementById("addresserrormsg").style.display = "none";
-  document.getElementById("dateerrormsg").style.display = "none";
-  document.getElementById("dateerrormsg").textContent =
-    "Please enter a valid birth date.";
+}
+
+function validateEmail(email) {
+  var emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegEx.test(email);
 }
