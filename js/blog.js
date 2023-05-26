06e5b96cfea1625870e79fc024e943447e75ff53
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-05-26 14:21:16 +0200

    added css to about page

--- js/blog.js
+++ js/blog.js
@@ -1,37 +1,3 @@
-// const detailContainer = document.querySelector("#js-results");
-
-// const url =
-//   "https://mikaela-exam.flywheelsites.com//wp-json/wp/v2/posts/?per_page=30";
-
-// async function fetchresults() {
-//   try {
-//     const response = await fetch(url);
-//     const results = await response.json();
-
-//     console.log(results);
-
-//     makeCards(results);
-//   } catch (error) {
-//     console.log(error);
-//     detailContainer.innerHTML = message("error", error);
-//   }
-// }
-
-// fetchresults();
-
-// function makeCards(postsArray) {
-//   const blogContainer = document.querySelector("#blog-container");
-//   console.log(postsArray);
-//   postsArray.forEach((post) => {
-//     blogContainer.innerHTML += `
-//       <a href="details.html?id=${post.id}" class="container">
-//         <h2>${post.title.rendered}</h2>
-//         <p class="Title">${post.excerpt.rendered}</p>
-//       </a>
-//     `;
-//   });
-// }
-
 const url =
   "https://mikaela-exam.flywheelsites.com//wp-json/wp/v2/posts/?per_page=30";
 async function fetchResults() {
